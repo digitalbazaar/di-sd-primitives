41333cfaabd68e21ab9141edbc39fd6c12637385
Dave Longley <dlongley@digitalbazaar.com>
2023-08-13 19:32:15 -0400

    Rework primitives to use skolemized JSON-LD.

--- test/primitives.spec.js
+++ test/primitives.spec.js
@@ -6,12 +6,36 @@ import * as primitives from '../lib/index.js';
 describe('exports', () => {
   it('it should have proper exports', async () => {
     should.exist(primitives);
-    primitives.deskolemize.should.be.a('function');
-    primitives.filterAndGroup.should.be.a('function');
-    primitives.group.should.be.a('function');
+    // skolemize
+    primitives.deskolemizeNQuads.should.be.a('function');
+    primitives.skolemizeCompactJsonLd.should.be.a('function');
+    primitives.skolemizeExpandedJsonLd.should.be.a('function');
+    primitives.skolemizeNQuads.should.be.a('function');
+    primitives.toDeskolemizedNQuads.should.be.a('function');
+    // canonicalize
+    primitives.canonicalize.should.be.a('function');
+    primitives.canonizeProof.should.be.a('function');
+    primitives.createHmacIdLabelMapFunction.should.be.a('function');
+    primitives.createLabelMapFunction.should.be.a('function');
+    primitives.hashCanonizedProof.should.be.a('function');
+    primitives.labelReplacementCanonicalizeJsonLd.should.be.a('function');
+    primitives.labelReplacementCanonicalizeNQuads.should.be.a('function');
+    primitives.relabelBlankNodes.should.be.a('function');
+    primitives.stripBlankNodePrefixes.should.be.a('function');
+    // group
+    primitives.canonicalizeAndGroup.should.be.a('function');
+    // hash
+    primitives.createHasher.should.be.a('function');
+    // hmac
+    primitives.createHmac.should.be.a('function');
+    // mandatory
     primitives.hashMandatory.should.be.a('function');
-    primitives.hmacIdCanonize.should.be.a('function');
-    primitives.skolemize.should.be.a('function');
-    primitives.toDeskolemizedRDF.should.be.a('function');
+    // select
+    primitives.selectJsonLd.should.be.a('function');
+    primitives.selectCanonicalNQuads.should.be.a('function');
+    // pointer
+    primitives.parsePointer.should.be.a('function');
+    // helpers
+    primitives.stringToUtf8Bytes.should.be.a('function');
   });
 });
